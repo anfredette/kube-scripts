1fee7e2b09f187968479aa9eddafc35cfadf8a37
Andre Fredette <anfredette@gmail.com>
2022-03-25 16:32:07 -0400

    Update create-nginx-svc.sh to support creating multiple services
    Signed-off-by: Andre Fredette <anfredette@gmail.com>

--- create-nginx-svc.sh
+++ create-nginx-svc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-kubectl create deployment nginx --image=nginx
-kubectl expose deployment nginx --port=80
-subctl export service --namespace default nginx
+kubectl create deployment nginx$1 --image=nginx
+kubectl expose deployment nginx$1 --port=80
+subctl export service --namespace default nginx$1
 
 sleep 2s
 echo "Check global ingress IP for nginx service"
-kubectl get globalingressip nginx
+kubectl get globalingressip nginx$1
