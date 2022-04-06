d381e5de62f9016e3407c347161e1117e0d418ff
Andre Fredette <anfredette@gmail.com>
2022-04-06 10:33:21 -0400

    Enable nettest.sh to deploy shell on control plane node
    Signed-off-by: Andre Fredette <anfredette@gmail.com>

--- nettest.sh
+++ nettest.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 # Create a pod with a shell in the default namespace from which network tests may be run.
-kubectl -n default run tmp-shell --rm -i --tty --image quay.io/submariner/nettest -- /bin/bash
+# If called with a cp node name, then attempt to schedule the shell on the given node
+# Assumes KUBECONFIG is set appropriately.
+
+
+if [ -z "$1" ]; then
+  kubectl -n default run tmp-shell --rm -i --tty --image quay.io/submariner/nettest -- /bin/bash
+elif [ $1 == "cluster1-control-plane" ]; then
+  kubectl -n default run tmp-shell --rm -i --tty --image quay.io/submariner/nettest --overrides='{"spec": { "nodeSelector": {"kubernetes.io/hostname": "cluster1-control-plane"}, "tolerations": [{"key": "node-role.kubernetes.io/master", "operator": "Exists","effect": "NoSchedule"}]}}' -- /bin/bash
+elif [ $1 == "cluster2-control-plane" ]; then
+  kubectl -n default run tmp-shell --rm -i --tty --image quay.io/submariner/nettest --overrides='{"spec": { "nodeSelector": {"kubernetes.io/hostname": "cluster2-control-plane"}, "tolerations": [{"key": "node-role.kubernetes.io/master", "operator": "Exists","effect": "NoSchedule"}]}}' -- /bin/bash
+else
+  echo "ERROR: Unsupported node name"
+fi
